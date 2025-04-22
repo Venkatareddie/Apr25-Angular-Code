a7821d88997d6d2190f9c2afadeb26c76bed1885
venkata <venkatareddy65852@gmail.com>
2025-04-22 15:39:24 -0400

    Day-02

---
+++ JavaScript/Day_02/addition.js
@@ -0,0 +1,7 @@
+
+let num1 = 10;
+let num2 = 20;
+
+console.log(`Addition of two numbers ${num1} and ${num2} is ${num1 + num2}`);
+console.log(`Multiplication of two numbers ` + num1 + ` and ` + num2 + ` is : ` + (num1 * num2));
+// window.alert(`Subtracting from ${num2} to ${num1} is ${num2 - num1}`);
