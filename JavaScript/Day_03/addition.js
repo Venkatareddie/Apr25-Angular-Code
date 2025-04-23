8a424068f86aadb47ec60d0828616493415f844b
venkata <venkatareddy65852@gmail.com>
2025-04-23 15:16:32 -0400

    Day_03 files are added

---
+++ JavaScript/Day_03/addition.js
@@ -0,0 +1,3 @@
+var a = 3;
+var b = 6;
+console.log("Addition of two numbers ".concat(a, " and ").concat(b, " is ").concat(a + b));
---
+++ JavaScript/Day_03/addition.ts
@@ -0,0 +1,4 @@
+let a: number = 3;
+let b: number = 6;
+
+console.log(`Addition of two numbers ${a} and ${b} is ${a + b}`);
